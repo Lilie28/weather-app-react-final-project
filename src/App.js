a3f3c3c3cf44e2d2436b58e84920640514213336
Lilie28 <Lilie28@users.noreply.github.com>
2021-02-15 13:57:59 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      Weather App
      <footer>
        This project was coded by <a href="https://www.linkedin.com/in/alice-arcourt-phd-5772728b/" target="_blank">Alice Arcourt</a>  and is {""}
      <a href="https://github.com/Lilie28/weather-app-react-final-project" target="_blank">
      open-sourced</a>
      </footer>
+       </div>
     </div>
   );
 }
