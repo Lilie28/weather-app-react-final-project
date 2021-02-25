2d298e7c1c99c09f8315d3dadbf948cff39b2f23
Lilie28 <Lilie28@users.noreply.github.com>
2021-02-25 15:07:23 +0000

    Integration of API

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Weather.css";
 
+
 export default function Weather(){
-let weatherData = {
-    city: "London",
+  const [ready, setReady]= useState (false);
+  const [weatherData, setWeatherData]=useState({});
+
+function handleResponse(response){
+  setWeatherData({
+    city: response.data.name,
     todayDate: "Sun 31 January",
     date: "today at 20:20",
-    weatherDescription: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-    currentTemperature: 10,
-    sunrise: "7:00",
-    sunset: "16:30",
-    humidity: 70,
-    wind: 3,
-    temperatureMax: 8,
-    temperatureMin: 0
-  };
+    weatherDescription: response.data.weather[0].description,
+    weatherIcon: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    currentTemperature: response.data.main.temp,
+    sunrise: response.data.sys.sunrise,
+    sunset: response.data.sys.sunset,
+    humidity: response.data.main.humidity,
+    wind: response.data.wind.speed,
+    temperatureMax: response.data.main.temp_max,
+    temperatureMin: response.data.main.temp_min
+  });
+  
+  setReady(true);
+
+}
+
+if (ready){
   return (
     <div className="Weather">
       <h6>
@@ -50,7 +62,7 @@ let weatherData = {
       <div className="temperature-settings">
         <div className="float-left">
           <span className="current-temperature">
-            {weatherData.currentTemperature}
+            {Math.round(weatherData.currentTemperature)}
           </span>
           <small className="units">
             <a href="#" className="active">
@@ -65,8 +77,8 @@ let weatherData = {
         Last updated: <span>{weatherData.date}</span>
       </h6>
 
-      <img className="imageForExample" src={weatherData.imgUrl} alt={weatherData.weatherDescription} />
-      <p className="current-weather-description">
+      <img className="imageForExample" src={weatherData.weatherIcon} alt={weatherData.weatherDescription} />
+      <p className="text-capitalize">
         {weatherData.weatherDescription}
       </p>
 
@@ -80,15 +92,15 @@ let weatherData = {
             </td>
             <td>
               {" "}
-              Temperature max: <span>{weatherData.temperatureMax}</span>˚C
+              Temperature max: <span>{Math.round(weatherData.temperatureMax)}</span>˚C
             </td>
           </tr>
           <tr>
             <td>
-              Wind: <span>{weatherData.wind} </span>km/h
+              Wind: <span>{Math.round(weatherData.wind)} </span>km/h
             </td>
             <td>
-              Temperature min: <span>{weatherData.temperatureMin}</span>˚C
+              Temperature min: <span>{Math.round(weatherData.temperatureMin)}</span>˚C
             </td>
           </tr>
 
@@ -102,7 +114,14 @@ let weatherData = {
      
     </div>
   );
-  
+}  else{
+ const apiKey="a156c6c640df016853c05d9f7e81abef";
+  let city= "London";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse); 
+
+  return "Loading...";
+}
 }
 
  
