2912b4ee93da34a2601e5789a0e986e7682938d5
Lilie28 <Lilie28@users.noreply.github.com>
2021-02-25 17:40:03 +0000

    Search engine updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,5 @@
 import React, {useState} from "react";
-import FormattedDate from "./FormattedDate";
-import FormattedTodayDate from "./FormattedTodayDate";
-import FormattedDateSunriseSunset from "./FormattedDateSunriseSunset";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
@@ -9,6 +7,7 @@ import "./Weather.css";
 export default function Weather(props){
   const [ready, setReady]= useState (false);
   const [weatherData, setWeatherData]=useState({});
+  const [city, setCity] = useState (props.defaultCity);
 
 function handleResponse(response){
   setWeatherData({
@@ -25,9 +24,22 @@ function handleResponse(response){
     temperatureMax: response.data.main.temp_max,
     temperatureMin: response.data.main.temp_min
   });
-  
   setReady(true);
+}
 
+function handleSubmit (event){
+  event.preventDefault();
+  search ();
+}
+
+function handleCityChange(event){
+  setCity (event.target.value);
+}
+
+function search(){
+  const apiKey="a156c6c640df016853c05d9f7e81abef";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse); 
 }
 
 if (ready){
@@ -40,11 +52,12 @@ if (ready){
         <span></span>
       </h6>
 
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="search"
           className="search-input"
           placeholder=" or enter a city"
+          onChange={handleCityChange}
           autoComplete="off"
         />
         <button type="submit" className="search-button" title="Search any city">
@@ -52,78 +65,13 @@ if (ready){
         </button>
       </form>
 
-      <h1 className="city">{weatherData.city}</h1>
-      <h5 className="todayDate"><FormattedTodayDate date={weatherData.date}/></h5>
-      <br />
-      <h6 className="sunrise-sunset">
-        Sunrise: <span><FormattedDateSunriseSunset date={weatherData.sunrise}/></span>
-        <br />
-        Sunset: <span><FormattedDateSunriseSunset date={weatherData.sunset}/></span>
-      </h6>
-      <br />
-
-      <div className="temperature-settings">
-        <div className="float-left">
-          <span className="current-temperature">
-            {Math.round(weatherData.currentTemperature)}
-          </span>
-          <small className="units">
-            <a href="#" className="active">
-              ˚C{" "}
-            </a>
-            |<a href="#">˚F </a>
-          </small>
-        </div>
-      </div>
-
-      <h6 className="updated">
-        Last updated: <span><FormattedDate date={weatherData.date}/></span>
-      </h6>
-
-      <img className="imageForExample" src={weatherData.weatherIcon} alt={weatherData.weatherDescription} />
-      <p className="text-capitalize">
-        {weatherData.weatherDescription}
-      </p>
-
-      <br />
-      <br />
-      <div className="parameters">
-        <table style={{ width: "100%" }}>
-          <tr>
-            <td>
-              Humidity: <span>{weatherData.humidity}</span>%
-            </td>
-            <td>
-              {" "}
-              Temperature max: <span>{Math.round(weatherData.temperatureMax)}</span>˚C
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Wind: <span>{Math.round(weatherData.wind)} </span>km/h
-            </td>
-            <td>
-              Temperature min: <span>{Math.round(weatherData.temperatureMin)}</span>˚C
-            </td>
-          </tr>
-
-          <td> </td>
-        </table>
-      </div>
-      <br />
-
-      <div className="row weather-forecast"></div>
-
-     
+      <WeatherInfo data={weatherData}/>
     </div>
   );
-}  else{
- const apiKey="a156c6c640df016853c05d9f7e81abef";
-  let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse); 
+  }else{
+  search ();
 
   return "Loading...";
+  
+  }
 }
-}
-
- 
---
+++ src/WeatherInfo.js
@@ -0,0 +1,73 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import FormattedTodayDate from "./FormattedTodayDate";
+import FormattedDateSunriseSunset from "./FormattedDateSunriseSunset";
+
+export default function WeatherInfo(props){
+return (
+  <div className="WeatherInfo">
+
+      <h1 className="city">{props.data.city}</h1>
+      <h5 className="todayDate"><FormattedTodayDate date={props.data.date}/></h5>
+      <br />
+      <h6 className="sunrise-sunset">
+        Sunrise: <span><FormattedDateSunriseSunset date={props.data.sunrise}/></span>
+        <br />
+        Sunset: <span><FormattedDateSunriseSunset date={props.data.sunset}/></span>
+      </h6>
+      <br />
+
+      <div className="temperature-settings">
+        <div className="float-left">
+          <span className="current-temperature">
+            {Math.round(props.data.currentTemperature)}
+          </span>
+          <small className="units">
+            <a href="#" className="active">
+              ˚C{" "}
+            </a>
+            |<a href="#">˚F </a>
+          </small>
+        </div>
+      </div>
+
+      <h6 className="updated">
+        Last updated: <span><FormattedDate date={props.data.date}/></span>
+      </h6>
+
+      <img className="imageForExample" src={props.data.weatherIcon} alt={props.data.weatherDescription} />
+      <p className="text-capitalize">
+        {props.data.weatherDescription}
+      </p>
+
+      <br />
+      <br />
+      <div className="parameters">
+        <table style={{ width: "100%" }}>
+          <tr>
+            <td>
+              Humidity: <span>{props.data.humidity}</span>%
+            </td>
+            <td>
+              {" "}
+              Temperature max: <span>{Math.round(props.data.temperatureMax)}</span>˚C
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Wind: <span>{Math.round(props.data.wind)} </span>km/h
+            </td>
+            <td>
+              Temperature min: <span>{Math.round(props.data.temperatureMin)}</span>˚C
+            </td>
+          </tr>
+
+          <td> </td>
+        </table>
+      </div>
+      <br />
+
+      <div className="row weather-forecast"></div>
+  </div>
+)
+}
